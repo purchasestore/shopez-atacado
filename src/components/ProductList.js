dc4b57600e7ea13142d548760e517ce497a24972
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-19 20:40:18 -0300

    fix the size of cart list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const ProductList = ({ addToCart }) => {
-  const [products, setProducts] = useState([
-    { name: 'Product 1', price: 10.99, size: ['PP','P','M'], image: 'https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-macacao-fechado-manga-comprida-tumblr--p-1656093927804.jpeg', quantity: 0, id: 1 },
-    { name: 'Product 2', price: 19.99, size: ['PP','P','M'], image: 'https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-macacao-fechado-manga-comprida-tumblr--p-1673444046549.png', quantity: 0, id: 2 },
-    { name: 'Product 3', price: 24.99, size: ['PP','P','M'], image: 'https://17741.cdn.simplo7.net/static/17741/sku/saia-saia-argola-sexy--p-1663959870905.jpeg', quantity: 0, id: 3 },
-    { name: 'Product 4', price: 14.99, size: ['PP','P','M'], image: 'https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-fio-com-argola-1676509827180.jpeg', quantity: 0, id: 4 },
-  ]);
+  const products = [
+    { id: 1, name: 'Product 1', price: 10.99, size: ['PP','P','M'], image: 'src/images/product_1.png', quantity: 0 },
+    { id: 2, name: 'Product 2', price: 19.99, size: ['PP','P','M'], image: 'src/images/product_2.png', quantity: 0 },
+    { id: 3, name: 'Product 3', price: 24.99, size: ['PP','P','M'], image: 'src/images/product_3.png', quantity: 0 },
+    { id: 4, name: 'Product 4', price: 14.99, size: ['PP','P','M'], image: 'src/images/product_4.png', quantity: 0 },
+  ];
 
-  const handleQuantityChange = (productId, e) => {
+  const [quantity, setQuantity] = useState({});
+  const [selectedSize, setSelectedSize] = useState({});
+
+  const handleQuantityChange = (id, e) => {
     const value = parseInt(e.target.value);
-    if (!isNaN(value) && value >= 1) {
-      const updatedProducts = products.map(product => {
-        if (product.id === productId) {
-          product.quantity = value;
-        }
-        return product;
-      });
-      setProducts(updatedProducts);
+    if (!isNaN(value)) {
+      setQuantity((prevQuantity) => ({ ...prevQuantity, [id]: value }));
     }
+  };
+
+  const handleSizeChange = (id, e) => {
+    const value = e.target.value;
+    setSelectedSize((prevSelectedSize) => ({ ...prevSelectedSize, [id]: value }));
   };
 
   return (
@@ -33,18 +35,18 @@ const ProductList = ({ addToCart }) => {
                 <h3 className="card-title">{product.name}</h3>
                 <span className="product-price h5">${product.price.toFixed(2)}</span>
                 <div className="form-group">
-                  <label htmlFor="size">Size</label>
-                  <select className="form-control" id="size">
+                  <label htmlFor={`size${product.id}`}>Size</label>
+                  <select className="form-control" id={`size${product.id}`} value={selectedSize[product.id]} onChange={(e) => handleSizeChange(product.id, e)}>
                     {product.size.map((size) => (
                       <option key={size} value={size}>{size}</option>
                     ))}
                   </select>
                 </div>
                 <div className="form-group">
-                  <label htmlFor="quantity">Quantity</label>
-                  <input type="number" className="form-control" key={product.id} id={`quantity${product.id}`} min="1" value={product.quantity} onChange={(e) => handleQuantityChange(product.id, e)} />
+                  <label htmlFor={`quantity${product.id}`}>Quantity</label>
+                  <input type="number" className="form-control" id={`quantity${product.id}`} min="1" value={quantity[product.id] || product.quantity} onChange={(e) => handleQuantityChange(product.id, e)} />
                 </div>
-                <button className="btn btn-primary" onClick={() => addToCart({ ...product, quantity: product.quantity, size: document.getElementById('size').value })} disabled={product.quantity < 1}>Add to cart</button>
+                <button className="btn btn-primary" onClick={() => addToCart({ ...product, quantity: quantity[product.id] || product.quantity, size: selectedSize[product.id] || product.size[0] })} disabled={(quantity[product.id] || product.quantity) < 1}>Add to cart</button>
               </div>
             </div>
           </div>
