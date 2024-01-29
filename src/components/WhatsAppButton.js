781fda62747871d978df4f03e62db5b12bcbc57e
PurchaseTester <158083126+PurchaseTester@users.noreply.github.com>
2024-01-29 15:30:34 -0300

    Alteração - Numero de WPP

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const WhatsAppButton = ({ message }) => {
-  const whatsappLink = `https://wa.me/+5511989070454?text=${encodeURIComponent(message)}`;
+  const whatsappLink = `https://wa.me/+5511913601927?text=${encodeURIComponent(message)}`;
 
   return (
     <a href={whatsappLink} className="btn btn-success" target="_blank" rel="noopener noreferrer">
