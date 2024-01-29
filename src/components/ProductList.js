924a2d8016a3390b5b3ab06f57c1e60d50f0e3dd
puppe1990 <matheus.puppe90@hotmail.com>
2024-01-29 12:56:21 -0300

    update list and add more images per product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,145 +4,246 @@ import PropTypes from 'prop-types';
 const ProductList = ({ addToCart }) => {
   const products = [
     {
-      "name": "VESTIDO COLADO MANGA FLARE FENDA",
-      "color": ["Preto", "Vermelho"],
+      "name": "CONJ-REVEILLON-TOP-SAIA-BRANCO",
+      "color": ["BRANCO"],
+      "price": 75.00,
       "size": ["PP", "P", "M", "G"],
-      "price": 55.90,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-colado-manga-flare-fenda-1690670625878.jpeg",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-conjunto-reveillon-com-top-decotado-e-saia-com-amarracao-colecao-premium-1703614371521.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "MACACÃO VIRGÍNIA TOP SUPLEX",
-      "color": ["Preto"],
+      "name": "CONJ-REVEILLON-TOP-SAIA-ROSA",
+      "color": ["ROSA"],
+      "price": 75.00,
       "size": ["PP", "P", "M", "G"],
-      "price": 55.90,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-macacao-decotado-manga-longa-suplex-1690477910840.png",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-conjunto-reveillon-com-top-decotado-e-saia-com-amarracao-colecao-premium-1703614394200.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "VESTIDO LÃZINHA OMBRO A OMBRO",
-      "color": ["Cinza", "Preto"],
-      "size": ["PP", "P", "M", "G", "GG"],
-      "price": 55.90,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-lazinha-ombro-a-ombro-1689086492234.jpeg",
-      "quantity": 0
-    },
-    {
-      "name": "VESTIDO MANGA ÚNICA COM FENDA GOLA DIAGONAL",
-      "color": ["Preto"],
-      "size": ["PP", "P", "M", "G", "GG"],
-      "price": 44.50,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-manga-unica-com-fenda-gola-diagonal-1689086420243.jpeg",
-      "quantity": 0
-    },
-    {
-      "name": "CONJUNTO TOP MANGA LONGA + SAIA COM FENDA",
-      "color": ["Preto", "Branco", "Vermelho", "Nude"],
-      "size": ["PP", "P", "M", "G", "GG"],
-      "price": 52.40,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-conjunto-saia-com-fenda-top-manga-longa-1689085146332.png",
-      "quantity": 0
-    },
-    {
-      "name": "CONJUNTO PAETÊ TOP CROPPED RETO ALCINHA + SAIA",
-      "color": ["Preto"],
+      "name": "CONJ-REVEILLON-TOP-SAIA - PRETO",
+      "color": ["PRETO"],
+      "price": 75.00,
       "size": ["PP", "P", "M", "G"],
-      "price": 44.90,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-conjunto-paete-top-cropped-reto-alcinha-saia-1689086350659.jpeg",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-conjunto-reveillon-com-top-decotado-e-saia-com-amarracao-colecao-premium-1703614385513.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "CONJUNTO LÃZINHA TOP E SAIA FENDA",
-      "color": ["Preto", "Cinza"],
-      "size": ["PP", "P", "M", "G", "GG"],
-      "price": 55.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-plus-conjunto-lazinha-top-cropped-gola-quadrada-e-saia-fenda-1689096753147.png",
-      "quantity": 0
-    },
-    {
-      "name": "MACACÃO FECHADO MANGA COMPRIDA TUMBLR",
-      "color": ["Preto"],
-      "size": ["PP", "P", "M", "G", "GG"],
-      "price": 59.90,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-macacao-fechado-manga-comprida-tumblr-p-1682695739878.jpeg",
-      "quantity": 0
-    },
-    {
-      "name": "Vestido Lãzinha Manga Longa Cacharrel",
-      "color": ["Preto", "Cinza"],
+      "name": "CONJ-REVEILLON-TOP-SAIA -NUDE",
+      "color": ["NUDE"],
+      "price": 75.00,
       "size": ["PP", "P", "M", "G"],
-      "price": 40.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-lazinha-ombro-a-ombro-manga-longa-cacharrel-1686934895859.png",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/conjuntos-conjunto-reveillon-com-top-decotado-e-saia-com-amarracao-colecao-premium-1703614362773.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "VESTIDO LONGO FENDA COM ARGOLA",
-      "color": ["Nude", "Preto", "Branco", "Vermelho"],
+      "name": "VEST-LONG-TOP-SAI-FEND - VERMELHO",
+      "color": ["VERMELHO"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-top-saia-fenda-1705337201985.jpg"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONG-TOP-SAI-FEND - NUDE",
+      "color": ["NUDE"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-top-saia-fenda-1705337199849.jpg"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONG-TOP-SAI-FEND - BRANCO",
+      "color": ["BRANCO"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-top-saia-fenda-1705337197474.jpg"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONG-TOP-SAI-FEND - PRETO",
+      "color": ["PRETO"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-top-saia-fenda-1705337195710.jpg"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONGO-BOJO-ROMANO - NUDE",
+      "color": ["NUDE"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-com-bojo-romano-1699378185659.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONGO-BOJO-ROMANO - BRANCO",
+      "color": ["BRANCO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-com-bojo-romano-1699378182561.jpg"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONGO-BOJO-ROMANO - PRETO",
+      "color": ["PRETO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-com-bojo-romano-1699378211552.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "VEST-LONGO-BOJO-ROMANO-VERMELHO",
+      "color": ["VERMELHO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-com-bojo-romano-1699378142154.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "CONJUNTO TOP MANGA LONGA- PRETO",
+      "color": ["PRETO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-conjunto-saia-com-fenda-top-manga-longa-1697638142642.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "CONJUNTO TOP MANGA LONGA- BRANCO",
+      "color": ["BRANCO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-conjunto-saia-com-fenda-top-manga-longa-1697638183579.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "CONJUNTO TOP MANGA LONGA- NUDE",
+      "color": ["NUDE"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-conjunto-saia-com-fenda-top-manga-longa-1697638153392.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "CONJUNTO TOP MANGA LONGA-VERMELHO",
+      "color": ["VERMELHO"],
+      "price": 45.00,
+      "size": ["PP", "P", "M", "G"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/vestido-conjunto-saia-com-fenda-top-manga-longa-1697638194332.png"
+      ],
+      "quantity": 0
+    },
+    {
+      "name": "BIQ-MARQUINHA-FIO- ROSA",
+      "color": ["ROSA"],
+      "price": 30.00,
       "size": ["PP", "P", "M", "G", "GG"],
-      "price": 44.50,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-fenda-com-argola-1690822101969.png",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-de-marquinha-de-fita-fio-duplo-com-calcinha-1699552531553.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "VESTIDO LONGO TOP + SAIA FENDA",
-      "color": ["Preto", "Branco", "Vermelho", "Nude"],
+      "name": "BIQ-MARQUINHA-FIO-NUDE",
+      "color": ["NUDE"],
+      "price": 30.00,
       "size": ["PP", "P", "M", "G", "GG"],
-      "price": 37.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/vestido-vestido-longo-top-saia-fenda-1690480891323.jpg",
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-kit-3-biquinis-de-marquinha-fita-com-fio-duplo--p-1696598601782.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "Biquini fio frente única",
-      "color": ["Preto", "azul", "pink", "laranja", "nude"],
-      "size": ["PP", "P", "M", "G"],
-      "price": 25.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-fio-frente-unica-1666295902446.jpeg",
+      "name": "BIQ-MARQUINHA-FIO-BRANCO",
+      "color": ["BRANCO"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G", "GG"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-kit-3-biquinis-de-marquinha-fita-com-fio-duplo--p-1696598217895.png"
+      ],
       "quantity": 0
     },
     {
-      "name": "BIQUÍNI FIO COM ARGOLA",
-      "color": ["Preto", "Laranja", "Pink", "Azul"],
-      "size": ["PP", "P", "M", "G"],
-      "price": 25.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-fio-com-argola-1666286988971.jpeg",
-      "quantity": 0
-    },
-    {
-      "name": "Biquini Cortininha De Amarrar",
-      "color": ["Pink", "Rosa-Bebê", "Preto", "Azul", "Laranja"],
-      "size": ["PP", "P", "M", "G"],
-      "price": 25.00,
-      "image": "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-cortininha-calcinha-de-amarrar-fio-1691423457455.jpeg",
+      "name": "BIQ-MARQUINHA-FIO-PRETO",
+      "color": ["PRETO"],
+      "price": 30.00,
+      "size": ["PP", "P", "M", "G", "GG"],
+      "images": [
+        "https://17741.cdn.simplo7.net/static/17741/sku/biquinis-biquini-de-marquinha-de-fita-fio-duplo-com-calcinha-1699552526534.png"
+      ],
       "quantity": 0
     }
   ]
   ;
+
   const [quantity, setQuantity] = useState({});
   const [selectedSize, setSelectedSize] = useState({});
   const [selectedColor, setSelectedColor] = useState({});
+  const [selectedImageIndex, setSelectedImageIndex] = useState({});
 
   const handleQuantityChange = (name, e) => {
-    const value = parseInt(e.target.value);
-    if (!isNaN(value)) {
-      setQuantity((prevQuantity) => ({ ...prevQuantity, [name]: value }));
-    }
+    const value = parseInt(e.target.value, 10);
+    setQuantity((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleColorChange = (name, e) => {
-    const value = e.target.value;
-    setSelectedColor((prevSelectedColor) => ({ ...prevSelectedColor, [name]: value }));
-    resetQuantity(name);
-  };  
-
-  const handleSizeChange = (name, e) => {
-    const value = e.target.value;
-    setSelectedSize((prevSelectedSize) => ({ ...prevSelectedSize, [name]: value }));
+    setSelectedColor((prev) => ({ ...prev, [name]: e.target.value }));
     resetQuantity(name);
   };
 
-  // Function to reset the quantity to 0 for a particular product
+  const handleSizeChange = (name, e) => {
+    setSelectedSize((prev) => ({ ...prev, [name]: e.target.value }));
+    resetQuantity(name);
+  };
+
   const resetQuantity = (name) => {
-    setQuantity((prevQuantity) => ({ ...prevQuantity, [name]: 0 }));
+    setQuantity((prev) => ({ ...prev, [name]: 0 }));
+  };
+
+  const handleImageChange = (name, direction) => {
+    setSelectedImageIndex((prevState) => {
+      const currentIndex = prevState[name] || 0;
+      const maxIndex = products.find((product) => product.name === name).images.length - 1;
+      let newIndex = currentIndex;
+
+      if (direction === 'next') {
+        newIndex = currentIndex < maxIndex ? currentIndex + 1 : 0;
+      } else {
+        newIndex = currentIndex > 0 ? currentIndex - 1 : maxIndex;
+      }
+
+      return { ...prevState, [name]: newIndex };
+    });
   };
 
   return (
@@ -152,7 +253,20 @@ const ProductList = ({ addToCart }) => {
           <div className="col-md-6 col-lg-4 mb-4" key={product.name}>
             <div className="card h-100">
               <div className="card-body">
-              <img src={product.image} alt={product.name} className="card-img-top" style={{width: "100%", height: "auto"}} />
+                <div className="product-image-container">
+                  <img
+                    src={product.images[selectedImageIndex[product.name] || 0]}
+                    alt={product.name}
+                    className="card-img-top"
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                  {product.images.length > 1 && (
+                    <>
+                      <button className="btn btn-secondary" onClick={() => handleImageChange(product.name, 'prev')}>Previous</button>
+                      <button className="btn btn-secondary" onClick={() => handleImageChange(product.name, 'next')}>Next</button>
+                    </>
+                  )}
+                </div>
                 <h5 className="card-title">{product.name}</h5>
                 <span className="product-price h5">R${product.price.toFixed(2)}</span>
                 <div className="form-group">
@@ -178,7 +292,7 @@ const ProductList = ({ addToCart }) => {
                   </select>
                 </div>
                 <div className="form-group">
-                  <label htmlFor={`quantity${product.name}`}>Quantidade</label>
+                  <label htmlFor={`quantity${product.name}`}>Quantity</label>
                   <input 
                     type="number" 
                     className="form-control" 
@@ -188,17 +302,17 @@ const ProductList = ({ addToCart }) => {
                   />
                 </div>
                 <button 
-                    className="btn btn-primary" 
-                    onClick={() => addToCart({ 
-                      ...product, 
-                      quantity: quantity[product.name] || 0, 
-                      size: selectedSize[product.name] || product.size[0], 
-                      color: selectedColor[product.name] || product.color[0] 
-                    })}
-                    disabled={!(quantity[product.name] > 0)}
-                  >
-                    Adicionar ao carrinho
-                  </button>
+                  className="btn btn-primary" 
+                  onClick={() => addToCart({ 
+                    ...product, 
+                    quantity: quantity[product.name] || 0, 
+                    size: selectedSize[product.name] || product.size[0], 
+                    color: selectedColor[product.name] || product.color[0] 
+                  })}
+                  disabled={!(quantity[product.name] > 0)}
+                >
+                  Adicionar ao carrinho
+                </button>
               </div>
             </div>
           </div>
