fe45be8873689c0c98c4eacea27570a2e1ab1671
Matheus Nunes Puppe <matheus.puppe90@hotmail.com>
2024-09-05 16:54:01 -0300

    Update WhatsAppButton.js

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const WhatsAppButton = ({ message }) => {
-  const whatsappLink = `https://wa.me/+5511913335676text=${encodeURIComponent(message)}`;
+  const whatsappLink = `https://wa.me/+5511913335676?text=${encodeURIComponent(message)}`;
 
   return (
     <a href={whatsappLink} className="btn btn-success" target="_blank" rel="noopener noreferrer">
